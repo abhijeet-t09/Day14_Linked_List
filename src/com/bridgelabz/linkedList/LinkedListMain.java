bf194d07843819af7d208f0f75c4abaaff4dd123
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-06 16:26:48 +0530

    Linked List: First Commit

---
+++ src/com/bridgelabz/linkedList/LinkedListMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Data Structures: Linked List");
+    }
+}
