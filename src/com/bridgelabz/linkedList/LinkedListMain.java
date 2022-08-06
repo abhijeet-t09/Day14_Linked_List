df03d4d4998a25324f69eacbea1549da22ace9d5
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-06 17:01:27 +0530

    Insert Element

--- src/com/bridgelabz/linkedList/LinkedListMain.java
+++ src/com/bridgelabz/linkedList/LinkedListMain.java
@@ -3,18 +3,25 @@ package com.bridgelabz.linkedList;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList1 = new LinkedList<>();
+//        linkedList1.push(70);
+//        linkedList1.push(30);
+//        linkedList1.push(56);
+//        linkedList1.print();
+//        System.out.println();
+//
+//        LinkedList<Integer> linkedList2 = new LinkedList<>();
+//
+//        linkedList2.add(56);
+//        linkedList2.add(30);
+//        linkedList2.add(70);
+//        linkedList2.print();
+
         linkedList1.push(70);
-        linkedList1.push(30);
         linkedList1.push(56);
         linkedList1.print();
         System.out.println();
 
-        LinkedList<Integer> linkedList2 = new LinkedList<>();
-
-        linkedList2.add(56);
-        linkedList2.add(30);
-        linkedList2.add(70);
-        linkedList2.print();
-
+        linkedList1.insert(30);
+        linkedList1.print();
     }
 }
