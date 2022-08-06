69d6bbb0abc2ee919c623fd162af77ee305efbb8
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-06 17:27:34 +0530

    Size of Linked List

--- src/com/bridgelabz/linkedList/LinkedListMain.java
+++ src/com/bridgelabz/linkedList/LinkedListMain.java
@@ -5,15 +5,14 @@ public class LinkedListMain {
         LinkedList<Integer> linkedList1 = new LinkedList<>();
 
         linkedList1.push(70);
+        linkedList1.push(30);
         linkedList1.push(56);
         linkedList1.print();
-
-        linkedList1.insert(30);
-        linkedList1.print();
-        System.out.println();
 
         linkedList1.insertAfter(30,40);
         linkedList1.print();
 
+        linkedList1.popElement(40);
+        System.out.println("Size of Linked List: " + linkedList1.size());
     }
 }
