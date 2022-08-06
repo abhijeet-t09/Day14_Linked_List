79c4abab6ae98631437ff454acf539eb447b4d12
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-06 16:35:13 +0530

    Adding Elememts

--- src/com/bridgelabz/linkedList/LinkedListMain.java
+++ src/com/bridgelabz/linkedList/LinkedListMain.java
@@ -2,6 +2,10 @@ package com.bridgelabz.linkedList;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        System.out.println("Data Structures: Linked List");
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+        linkedList.print();
     }
 }
